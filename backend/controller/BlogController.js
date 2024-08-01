a13e17e9aa28fb462cd07ba402c42e9a77fe73df
priyaK999 <priya@centennialinfotech.com>
2024-08-01 11:25:11 +0530

    Update BlogController.js

--- backend/controller/BlogController.js
+++ backend/controller/BlogController.js
@@ -28,33 +28,6 @@ exports.createBlog = async (req, res) => {
 }
 };
 
-exports.uploadImage = async (req, res) => {
-  const  image  = req.file;
-  console.log("image", req.file)
-
-  try {
-    if (!image) {
-      return res.status(400).json({
-        success: false,
-        message: 'Please provide an image',
-      });
-    }
-
-    // Upload the image to Cloudinary
-    const imageUrl = await uploadImageFromBase64(image);
-
-    res.status(200).json({
-      success: true,
-      imageUrl,
-    });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({
-      success: false,
-      message: 'Server error',
-    });
-  }
-};
 
 
 // Delete a blog by ID
@@ -79,44 +52,6 @@ exports.deleteBlog = async (req, res) => {
   }
 };
 
-// exports.createBlog = async (req, res) => {
-//   const { category, name, username, description,image } = req.body;
-
-
-//   try {
-//     if (!category || !name || !username || !description || !image) {
-//       return res.status(400).json({
-//         success: false,
-//         message: 'Please provide all required fields',
-//       });
-//     }
-
-//     // Upload the image to Cloudinary
-//     // const result = await cloudinary.uploader.upload(image, {
-//     //   folder: 'blog_images', // Optional: specify a folder in Cloudinary
-//     // });
-
-//     // Create the blog post with the image URL from Cloudinary
-//     const savedBlog = await Blog.create({
-//       category,
-//       name,
-//       username,
-//       description,
-//       image,
-//     });
-
-//   return  res.status(200).json({
-//       success: true,
-//       message: savedBlog,
-//     });
-//   } catch (error) {
-//     console.error('Error:', error);
-//     res.status(500).json({
-//       success: false,
-//       message: 'Server error',
-//     });
-//   }
-// };
 
 exports.getBlogs = async (req, res) => {
   try {
@@ -129,17 +64,12 @@ exports.getBlogs = async (req, res) => {
     }
 
     // Fetch the latest two blog posts in ascending order (oldest first) based on the category
-    const latestTwoBlogs = await Blog.find(filter).sort({ createdAt: 1 }).limit(2);
+    const latestTwoBlogs = await Blog.find(filter).sort({ createdAt: -1 });
 
-    // Fetch the rest of the blog posts in descending order (newest first) based on the category
-    const restOfBlogs = await Blog.find(filter).sort({ createdAt: -1 }).skip(2);
-
-    // Concatenate the two results
-    const allBlogs = latestTwoBlogs.concat(restOfBlogs);
 
     res.status(200).json({
       success: true,
-      data: allBlogs
+      data: latestTwoBlogs
     });
   } catch (error) {
     console.error('Error:', error);
@@ -149,5 +79,3 @@ exports.getBlogs = async (req, res) => {
     });
   }
 };
-
-

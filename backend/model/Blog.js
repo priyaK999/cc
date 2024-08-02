b846ca5735dccec3ff696d62a14ee0d466f532b1
priyaK999 <priya@centennialinfotech.com>
2024-08-02 10:08:09 +0530

    Update Blog.js

--- backend/model/Blog.js
+++ backend/model/Blog.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-// Define the schema for a blog post
+
 const blogSchema = new mongoose.Schema({
   category: {
     type: String,
-    required: true, // Makes this field required
+    required: true, 
   },
   name: {
     type: String,
@@ -18,18 +18,22 @@ const blogSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  detailedDescription: {
+    type: String,
+    required: true, 
+  },
   image: {
-    type: String, // This could be a URL or base64 encoded image string
+    type: String, 
     required: false,
   },
   createdAt: {
     type: Date,
-    default: Date.now, // Sets a default value to the current date
+    default: Date.now, 
   },
 });
 
-// Create a model using the schema
+
 const Blog = mongoose.model('Blog', blogSchema);
 
-// Export the model
+
 module.exports = Blog;
